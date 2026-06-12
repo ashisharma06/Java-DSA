eaaad1d92d7de03a253f30385137bdb8442b3c10
Ashish Sharma <ashish060206@gmail.com>
2026-06-12 10:40:00 +0530

    Calculated the Nth Fibonacci number

---
+++ Conditions-Loops/Fibo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Fibo {
+    public static void main(String[] args) {
+        // Scanner input = new Scanner(System.in);
+        // int n = input.nextInt();
+        // int a = 0;
+        // int b = 1;
+        // int count = 2;
+
+        // while (count <= n) {
+        //     int temp = b;
+        //     b = b + a;
+        //     a = temp;
+        //     count++;
+        // }
+
+        // System.out.println(b);
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+        while (count <= n) {
+            int next =  a + b;
+            a = b;
+            b = next;
+            count++;
+        }
+
+        System.out.println(b);
+    }
+}
