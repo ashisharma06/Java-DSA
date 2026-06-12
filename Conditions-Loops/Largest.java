a20ee6f08a667d17d10f1d28692dc4bde4f3b1c5
Ashish Sharma <ashish060206@gmail.com>
2026-06-12 10:20:16 +0530

    Program to find the largest of three numbers

---
+++ Conditions-Loops/Largest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        // Q. find the largest of the 3 numbers
+        int max = a;
+        if(b > max){
+            max = b;
+        }
+        if (c > max) { 
+            max = c;
+        }
+
+        System.out.println("Maximum value :"+ max);
+    }
+}
---
+++ Conditions-Loops/Loops.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        /*
+         * Syntax of for loops:
+         * for(initialization; condition; increment/decrement; ){
+         * // body
+         * }
+         */
+
+        // Q: print numbers from 1 to 5
+        for (int num = 1; num <= 5; num++) {
+            System.out.println(num);
+        }
+
+
+        // print numbers from 1 to n
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int num = 1; num <= n; num++) {
+            System.out.println(num + " ");
+        }
+
+        // while loops
+        /*
+            Syntax:
+            while(condition){
+                body
+            }
+         */
+
+         int num = 1;
+         while (num <= 5) {
+             System.out.println(num);
+             num++;
+         }
+
+        // do while loop
+        /*
+            Syntax:
+            do {
+                 body
+            } while (condition) {
+
+             }
+         */
+        int n = 1;
+        do {
+            System.out.println(n);
+            n++;
+        } while (n <= 5);
+    }
+}
