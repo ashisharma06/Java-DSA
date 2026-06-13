e628dc64390fae5c137bb03923f2e519cbf42cce
Ashish Sharma <ashish060206@gmail.com>
2026-06-13 10:45:30 +0530

    Learning basic Java methods and return types

---
+++ Greetings.java
@@ -0,0 +1,9 @@
+public class Greetings {
+    public static void main(String[] args) {
+        greeting();
+    }
+
+    static void greeting() {
+        System.out.println("Hello World!!");
+    }
+}
